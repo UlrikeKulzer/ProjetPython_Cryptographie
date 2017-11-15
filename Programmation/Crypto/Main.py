001107983dce203409abf56d1abf93ad6e8c0e11
Ulrike <ulrike.kulzer@uni-ulm.de>
2017-11-15 15:23:56 +0100

    small changements

--- Programmation/Crypto/Main.py
+++ Programmation/Crypto/Main.py
@@ -1,6 +1,3 @@
-# author: Ulrike Kulzer
-# date: 15/11/2017
-
 class Main:
     def main():
         print("This is the main class.")
--- Programmation/Crypto/screen.py
+++
@@ -1,4 +0,0 @@
-# author: Ulrike Kulzer
-# date: 15/11/2017
-
-class Screen:
