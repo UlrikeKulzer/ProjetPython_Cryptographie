8f971b26bae6001149f638890b577576d5212a16
Ulrike <ulrike.kulzer@uni-ulm.de>
2017-11-15 16:22:06 +0100

    test

--- Programmation/Crypto/Main.py
+++ Programmation/Crypto/Main.py
@@ -1,6 +1,23 @@
+class Screen():
+
+
+
 class Main:
+    screen = Screen()
+    english = True
+    encryption = False
+    caesar = False
+    vigenere = False
+    enigma = False
+    keyCaesar = 'abc'
+    keyVigenere = 'abc'
+    keyEnigma = 'abc'
+
+
+
     def main():
         print("This is the main class.")
+
 
 
 
--- Programmation/Crypto/Screen.py
+++ Programmation/Crypto/Screen.py
@@ -1,1 +1,42 @@
-class Screen:
+class Screen(object):
+    sth = ""
+
+    def __init__(self, sth):
+        self.sth = sth
+
+
+    def showStartAskLanguage():
+
+
+
+    def showMainMenu():
+
+
+
+    def showPrinciples():
+
+
+
+    def showAskKey():
+
+
+
+    def showAskText():
+
+
+
+    def showTreatedText():
+
+
+
+    def showLanguageSettings():
+
+
+
+    def showHelpPrinciples():
+
+
+
+    def showQuitMessage():
+
+
