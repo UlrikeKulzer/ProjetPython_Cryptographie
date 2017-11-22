58ff27c3b5a5b901eb85b4ceece0b785167e3395
Fathima SAHADATTALY <fathima.sahadattaly@edu.esiee.fr>
2017-11-22 15:52:37 +0100

    ajout fonction encrypt_caesar(text,key) et decrypt_caesar(text,key)

--- Programmation/text_module.py
+++ Programmation/text_module.py
@@ -15,8 +15,27 @@
 
 
     def encrypt_caesar(text, key):
+        crypted_text = ''  # chaine vide dans laquelle on va ajouter au fur et à mesure le texte crypté
+        for i in text:
+            if ord(i) + key > 90:  # si en ajoutant la clé au numéro de la lettre on dépasse le numéro de Z on doit revenir au début de l'alphabet
+                i = chr(ord('A') - 1 + (key - (ord('Z') - ord(i))))
+                crypted_text += i
+            else:  # sinon on opère de manière normale
+                i = chr(ord(i) + key)
+                crypted_text += i
+        return crypted_text
 
     def decrypt_caesar(text, key):
+        decrypted_text=''       #chaine vide dans laquelle on va ajouter au fur et à mesure le texte décrypté
+        for i in text:
+            if ord(i)-key<65:  #si en soustrayant la clé au numéro de la lettre on trouve un numéro inférieur à celui de A on doit repartir à la fin de l'alphabet
+                i=chr(ord('Z')+1-(key-(ord(i)-ord('A'))))
+                decrypted_text+=i
+            else:
+                i=chr(ord(i)-key)
+                decrypted_text+=i
+        return decrypted_text
+
 
     def encrypt_vigenere(text, key):
         vigenere_table = [][]
